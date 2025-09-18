eaba8e447e045b5b45576db3e4fc7794a30c4a5d
Shift <shift@laravelshift.com>
2025-09-18 19:31:38 +0000

    Shift cleanup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 use App\Exports\RegistrationExport;
 use App\Exports\RegistrationExportLast;
 use App\Models\Registrant;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AdminController extends Controller
 {
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\View\View;
 use App\Models\Registrant;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 // use App\Http\Requests;
 
