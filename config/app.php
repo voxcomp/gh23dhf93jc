8167555150fd66cd8e5ca3a078e77b0be472f427
Shift <shift@laravelshift.com>
2025-09-18 19:55:07 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,24 +7,6 @@ return [
 
     'timezone' => 'America/Chicago',
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        Srmklive\PayPal\Providers\PayPalServiceProvider::class,
-        NZTim\Mailchimp\MailchimpServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
